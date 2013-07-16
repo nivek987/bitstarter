9270d269884a85094a55fd193af9bf5000845f1e
Kevin Schouweiler <nivek987@gmail.com>
2013-07-16 07:49:41 +0000

    Added first attempt to edit Web.js using fs.Readfile and buffer.toString

--- web.js
+++ web.js
@@ -4,8 +4,11 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-});
+  response.send(fs.readFile('/index.html', function (err, data) {// #This addresses my fs.readFile variable
+	  if (err) throw err;
+	  console.log(data + buf.toString);// #And this is for my buffer to splice in the data from this index.html file
+  });
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
