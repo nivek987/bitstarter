f2859e97abbc3b82407e198755a19ca11b7c3430
Kevin Schouweiler <nivek987@gmail.com>
2013-07-17 01:24:00 +0000

    Update to web.js for fs.readFile and Buffer for finding data on index.html

--- web.js
+++ web.js
@@ -1,11 +1,17 @@
-var buffer = new Buffer("utf-8")
+var express = require ('express'), fs = require('fs');
 
+var index_data = new Buffer(16);
 
-fs.readFile('/index.html', function (err, data) {// #This addresses my fs.readFile variable
+fs.readFile('index.html', function (err, data) {// #This addresses my fs.readFile variable
 	  if (err) throw err;
-	  console.log(fs.readFile + buf.toString);// #And this is for my buffer to splice in the data from this index.html file
+	  index_data = data;
   });
 
+var app = express.createServer(express.logger());
+
+app.get ('/', function(request, response) {
+response.send(index_data.toString());
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
